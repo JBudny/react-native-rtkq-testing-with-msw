1c2407a0fa7e5fbe1c5f58aaaedbaf23de982653
jbudny <jbudny8@gmail.com>
2021-11-26 01:05:58 +0100

    Clear repository

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+    </SafeAreaView>
+  );
+};
+
+export default App;
