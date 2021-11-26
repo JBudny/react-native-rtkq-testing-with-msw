e48ccbf856755ab32fea29c136399e310a4d837c
jbudny <jbudny8@gmail.com>
2021-11-26 05:44:15 +0100

    Setup testing environment

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
-import {store} from './src/store';
+import {setupStore} from './src/store';
+
+const store = setupStore();
 
 const AppWrapper = () => (
   <Provider store={store}>
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,22 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import type {PreloadedState} from '@reduxjs/toolkit';
 
 import {api} from './services/api';
 
-export const store = configureStore({
-  reducer: {
-    [api.reducerPath]: api.reducer,
-  },
-  middleware: gDM => gDM().concat(api.middleware),
+const rootReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware =>
+      // adding the api middleware enables caching, invalidation, polling and other features of `rtk-query`
+      getDefaultMiddleware().concat(api.middleware),
+    preloadedState,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
